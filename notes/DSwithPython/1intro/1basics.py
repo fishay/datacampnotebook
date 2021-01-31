8335d6279012338870680a7e0b0a2fd9238cff74
fishay <44349120+fishay@users.noreply.github.com>
2021-01-31 16:04:07 -0500

    Update 1basics.py

--- notes/DSwithPython/1intro/1basics.py
+++ notes/DSwithPython/1intro/1basics.py
@@ -1,1 +1,4 @@
-
+#for comment
+Exponentiation: **. This operator raises the number to its left to the power of the number to its right. For example 4**2 will give 16.
+Modulo: %. This operator returns the remainder of the division of the number to the left by the number on its right. For example 18 % 7 equals 4.
+18= 7*2+4
