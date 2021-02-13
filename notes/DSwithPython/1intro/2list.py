ab1f94442ab33343386705fe05d1db5f1466cb68
fishay <44349120+fishay@users.noreply.github.com>
2021-02-13 17:44:37 -0500

    Update 2list.py

--- notes/DSwithPython/1intro/2list.py
+++ notes/DSwithPython/1intro/2list.py
@@ -1,1 +1,12 @@
+x = ["a", "b", "c", "d"]
+x[1:3]
+return ["b","c"]
+x[1:] = ["b","c","d"]
+del(x[])
+shallow copy 
+deep copy for list:
+y = list(x)
+y = [:]
 
+x = ["a", "b", "c", "d"]
+y = x + ["e", "f"]
