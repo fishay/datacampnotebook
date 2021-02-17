7d2ea01ebcd164d1d7a2acf11a5c5280ca9fe48f
fishay <44349120+fishay@users.noreply.github.com>
2021-02-16 22:23:01 -0500

    Update 3FunctionsNPackages.py

--- notes/DSwithPython/1intro/3FunctionsNPackages.py
+++ notes/DSwithPython/1intro/3FunctionsNPackages.py
@@ -1,1 +1,22 @@
+round(1.68,1)
+return 1.7
+round(1.68)
+return 2
+help(round)
+help(max)
+or ?max
 
+append(), that adds an element to the list it is called on,
+remove(), that removes the first element of a list that matches the input, and
+reverse(), that reverses the order of the elements in the list it is called on.
+
+Packages:
+Installpackagehp://pip.readthedocs.org/en/stable/installing/
+Download
+  get-pip.py
+Terminal:
+  python3 get-pip.py
+  pip3 install numpy
+  
+from numpy import array
+array([1, 2, 3])
